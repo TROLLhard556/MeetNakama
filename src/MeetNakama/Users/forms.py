721a1aca2fd1494d2e2e0cfc3ff2a467b33ed3eb
TROLLhard556 <trollhard556@gmail.com>
2021-07-17 21:17:50 +0000

    removed help text from signup page

--- src/MeetNakama/Users/forms.py
+++ src/MeetNakama/Users/forms.py
@@ -4,6 +4,12 @@ from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
 
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+        for fieldname in ['email', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
+
     class Meta:
         model = CustomUser
         fields = ('email', 'password1', 'password2')
